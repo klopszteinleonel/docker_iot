34880cdf35dc9d6fa3c3f21e6e66577215e189a6
Leonel <klopszteinleonel@gmail.com>
2026-06-11 15:09:54 -0300

    agrega selector de tema

--- crud/static/js/main.js
+++ crud/static/js/main.js
@@ -10,3 +10,13 @@ if(btnDelete) {
     });
   })
 }
+
+function setTheme(theme) {
+  localStorage.setItem('theme', theme);
+  const themeLink = document.getElementById('theme-stylesheet');
+  if (theme === 'oscuro') {
+    themeLink.href = 'https://bootswatch.com/5/darkly/bootstrap.min.css';
+  } else {
+    themeLink.href = 'https://bootswatch.com/5/cosmo/bootstrap.min.css';
+  }
+}
